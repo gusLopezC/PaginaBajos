24c2508b5514cac2b41a334aa645525192e4aca2
desarrolloremo <gustavo.lopez@remo.mx>
2019-03-22 13:26:54 -0600

    COrreccion

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
       <div class="social-links float-right">
 
         <a href="#" class="facebook">
-          <i class="fa fa-facebook"></i> Normas Bajos</a>
+          <i class="fa fa-facebook"></i>@BajosAcapulco</a>
 
       </div>
     </div>
@@ -114,24 +114,24 @@
 
       <div class="section-header">
         <h2>ALTA
-          <span> TECNOLOGÍA</span> A TU SERVICIO</h2>
+          <span> TECNOLOGÍA</span> A SU SERVICIO</h2>
       </div>
       <div>
 
       </div>
       <div class="container-counter">
-        <div class="section-option col-sm-6" style=" background: rgb(255, 255, 255, 0.5);padding: 60px; ">
+        <div class="section-option col-sm-6" style=" background: rgb(255, 255, 255, 0.8);padding: 161px; ">
 
           <h2>BIENVENIDOS A
             <br>
-            <span>BAJOS ACAPULCO</span>
+            <span>MICROPROCESAMIENTOS</span>
           </h2>
           <p>Somos una compañia mexicana con más de 35 años de experiencia,dedicada a la comercialización,servicio e instalación
             de los mejores productos de alta tecnología en comunicaciones y procesamiento de datos para oficinas,corportivos
             e instituciones públicas y privadas.
           </p>
         </div>
-        <div class="section-option col-sm-6" style=" background: rgb(175, 199, 244, 0.5);padding: 103px; ">
+        <div class="section-option col-sm-6" style=" background: rgb(175, 199, 244, 0.8);padding: 214px; ">
           <h2>
             <span>MISIÓN</span>
           </h2>
@@ -139,7 +139,7 @@
           </p>
         </div>
 
-        <div class="col-sm-4" style=" background: rgb(12, 46, 138 , 0.5); ">
+        <div class="col-sm-4" style=" background: rgb(12, 46, 138 , 0.8); ">
           <div class="counter">
             <h1 class="counter-number">
               <div id="number1" class="counter-timer" data-from="1950" data-to="1981">1981</div>
@@ -150,7 +150,7 @@
             </h1>
           </div>
         </div>
-        <div class="col-sm-4" style=" background: rgb(255, 255, 255, 0.5); ">
+        <div class="col-sm-4" style=" background: rgb(255, 255, 255, 0.8); ">
           <div class="counter">
             <h1 class="counter-number">
               <div id="number2" class="counter-timer" style="color: #0c2e8a" data-from="0" data-to="25">25</div>
@@ -158,7 +158,7 @@
             <h1 class="counter-title" style="color: #0c2e8a;">Marcas han confiado en nosotros</h1>
           </div>
         </div>
-        <div class="col-sm-4" style=" background: rgb(12, 46, 138 , 0.5); ">
+        <div class="col-sm-4" style=" background: rgb(12, 46, 138 , 0.8); ">
           <div class="counter">
             <h1 class="counter-number">
               <div id="number3" class="counter-timer" data-from="0" data-to="38">38</div>
@@ -289,74 +289,9 @@
       <div style=" background: rgb(2, 12, 68, 0.9)">
         <div class="container">
           <div class="row">
-            <div class="col-sm-4">
-              <br>
-              <table class='tablacontacto table borderless'>
-                <tr>
-                  <td>
-                    <strong>Localización</strong>
-                    <br> Quebrada 62, Col. Centro
-                    <br> Junto al Teatro Domingo
-                    <br> Soler, Acapulco, Gro.
-                    <br> C.P. 39300
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <strong>Contacto</strong>
-                    <br> Tel. 744 482 1938
-                    <br> micropro@btu.com.mx
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <strong>Horario de atención</strong>
-                    <br> Lunes - Viernes
-                    <br> 8:30 am - 18:00 pm
-                  </td>
-                </tr>
-
-              </table>
-            </div>
-            <div class="col-sm-8">
-              <div class="map-responsive">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3818.5701845112967!2d-99.91496148561484!3d16.84766582258168!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85caf79ab08d5637%3A0xc4d81b0ad505a0d0!2sBajos+Servicios+de+Impresi%C3%B3n!5e0!3m2!1ses-419!2smx!4v1552868195412"
-                  width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
-              </div>
-            </div>
-
-          </div>
-          <br>
-          <br>
-          <div class="container">
-            <div class="form">
-              <div id="sendmessage">Tu mensaje ha sido enviado. ¡Gracias!</div>
-              <div id="errormessage"></div>
-              <form action="" method="post" role="form" class="contactForm">
-                <div class="form-row">
-                  <div class="form-group col-md-6">
-                    <input type="text" name="name" class="form-control" id="name" placeholder="Nombre" data-rule="minlen:4" data-msg="Por favor ingrese tu nombre">
-                    <div class="validation"></div>
-                  </div>
-                  <div class="form-group col-md-6">
-                    <input type="email" class="form-control" name="email" id="email" placeholder="Correo" data-rule="email" data-msg="Por favor introduzca una dirección de correo electrónico válida">
-                    <div class="validation"></div>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <input type="text" class="form-control" name="subject" id="subject" placeholder="Asunto" data-rule="minlen:4" data-msg="Por favor ingrese el asunto">
-                  <div class="validation"></div>
-                </div>
-                <div class="form-group">
-                  <textarea class="form-control" name="message" rows="5" data-rule="required" data-msg="Por favor escribe algo para nosotros"
-                    placeholder="Mensaje"></textarea>
-                  <div class="validation"></div>
-                </div>
-                <div class="text-center">
-                  <button type="submit">Enviar Mensaje</button>
-                </div>
-              </form>
-            </div>
+           <br>
+           <br>
+           <br>
 
           </div>
         </div>
